cdb410c79e863a71022baf31521296ecfb1e717d
Andrew Rogers <andrewprogers@gmail.com>
2024-04-01 17:52:17 -0700

    add pip to build

--- build.zsh
+++ build.zsh
@@ -4,5 +4,6 @@
   && rm -rf ../build \
   && cp -R ./build ../build \
 ) && \
+pip freeze > requirements.txt
 gcloud app deploy --project=cross-react --quiet \
 && curl https://cross-react.uw.r.appspot.com/api/user/
